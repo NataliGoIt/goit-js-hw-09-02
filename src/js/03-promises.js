87d39401688bba9d25c9d0ffdd0c3e83bfc4b804
NataliGoIt <nataliakal4enko@gmail.com>
2023-03-08 11:20:32 +0200

    promise

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,33 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+import Notiflix from "notiflix";
+const form = document.querySelector(".form");
+form.addEventListener("submit", onFormSubmit);
+
+function onFormSubmit(evt) {
+  evt.preventDefault();
+  const { delay, step, amount } = evt.currentTarget;
+
+  let dalayPromise = Number(delay.value);
+
+  for (let i = 1; i <= amount.value; i++) {
+    createPromise(i, dalayPromise).then(onResolve).catch(onRejected);
+    dalayPromise += Number(step.value);
   }
+}
+function createPromise(position, delay) {
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      }
+      reject(`❌ Rejected promise ${position} in ${delay}ms`);
+    }, delay);
+  });
+}
+function onResolve(result) {
+  Notiflix.Notify.success(result);
+}
+
+function onRejected(error) {
+  Notiflix.Notify.failure(error);
 }
